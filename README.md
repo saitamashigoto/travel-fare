45d64296c04fc27fd9e02c0897a1cc361610eaaf
Saitama <piyushgoelshigoto@gmail.com>
2021-12-20 21:24:01 +0530

    Done.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Travel Fare Application
-Travel Fare
+- You need apache or nginx + php fpm to run this app.
+- Clone the repository in your document root.
+- Open http://you.host.name.or.ip.address/travel-fare/index.php in your web browser.
--- controller/fare/upload.php
+++ controller/fare/upload.php
@@ -4,13 +4,19 @@ require '../../src/Piyush/autoload.php';
 
 use Piyush\Model\FareManagement;
 
+$mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+
 if(
     isset($_POST["upload"]) &&
     isset($_POST["upload"]) &&
     isset($_FILES["fareData"]) &&
     $_FILES["fareData"]['error'] === UPLOAD_ERR_OK
 ) {
-    FareManagement::calculateCheapestFaresFromCsvFile($_FILES["fareData"]['tmp_name']);
-}
+    if (in_array($_FILES['fareData']['type'], $mimes)) {
+        FareManagement::calculateCheapestFaresFromCsvFile($_FILES["fareData"]['tmp_name']);
+    } else {
+        die('File not supported.');
+    }
+} 
 
 header('Location: /travel-fare/view/fare/index.php');
